58b07867358d3116b8e5fb88b15ae83637337e4b
dkowsikpai <dkowsikpai@gmail.com>
2019-12-10 09:04:25 +0530

    Updates

---
+++ precache-manifest.a612e4a39e8fa5ee0d00a2fa6f4f73b5.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "6afaa346915b66a608cea5d217d7e812",
+    "url": "/webteam.github.io/index.html"
+  },
+  {
+    "revision": "5e8d40f8fd825f73e286",
+    "url": "/webteam.github.io/static/css/main.05a63d84.chunk.css"
+  },
+  {
+    "revision": "b36df4ffc8500d6ac2a7",
+    "url": "/webteam.github.io/static/js/2.87f0119c.chunk.js"
+  },
+  {
+    "revision": "52ece5cef12d2ddcff30135306197eeb",
+    "url": "/webteam.github.io/static/js/2.87f0119c.chunk.js.LICENSE"
+  },
+  {
+    "revision": "5e8d40f8fd825f73e286",
+    "url": "/webteam.github.io/static/js/main.060ec40b.chunk.js"
+  },
+  {
+    "revision": "09af55e36db008fda899",
+    "url": "/webteam.github.io/static/js/runtime-main.2a331766.js"
+  },
+  {
+    "revision": "c36298e42559ae117696aab1ef89d710",
+    "url": "/webteam.github.io/static/media/web2.c36298e4.jpeg"
+  }
+]);
